f9bef69ade920d77e75207e009cf91bae7237e47
Agewiser <jimiakinleye@gmail.com>
2026-02-23 17:53:34 +0000

    open CORS for vercel deployment

--- server/index.js
+++ server/index.js
@@ -12,12 +12,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [
-      "http://localhost:5173",
-      "http://127.0.0.1:5500",
-      "http://localhost:5500",
-      "http://localhost:3000",
-    ],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
